7024d205eee8824830591413c63e70622eef1581
Alex <best1972@list.ru>
2023-01-20 19:43:56 +0300

    change in task 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
 //строго больше 0 ввёл пользователь. 0, 7, 8, -2, -2 -> 2,  0, 1, -7, 567, 89, 223-> 4
-/*Console.WriteLine("Введите M чисел через пробел: ");
-string X = Console.ReadLine()!;;
-string[] num = X.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+/*Console.Write("Введите M чисел через пробел: ");
+string M = Console.ReadLine()!;
+string[] num = M.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 int count = 0;
 foreach (string numX in num)
 if(Convert.ToInt32(Convert.ToString(numX)) > 0) count = count + 1;
 else count = count * 1;
-Console.WriteLine($"Пользователь ввел: {count} больше 0");*/ 
+Console.Write($"Чисел больше 0 пользователь ввел: {count}");*/
   
 
