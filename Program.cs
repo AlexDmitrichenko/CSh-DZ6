b21e55b4fba27aec53528f08f40627f755d1ab7e
Alex <best1972@list.ru>
2023-01-22 12:02:31 +0300

    change in task 2

--- Program.cs
+++ Program.cs
@@ -8,5 +8,37 @@ foreach (string numX in num)
 if(Convert.ToInt32(Convert.ToString(numX)) > 0) count = count + 1;
 else count = count * 1;
 Console.Write($"Чисел больше 0 пользователь ввел: {count}");*/
-  
+//Задача 43: Написать программу, которая на вход принимает массив из любого количества 
+//элементов (не менее 6) в промежутке от 0 до 100, а на выходе выводит этот же массив, 
+//но отсортированный по возрастанию(от меньшего числа к большему).
+int[] arr = GetArray(8);
+Console.WriteLine($"{String.Join(" ", arr)}");
+//Method--------------------------------------------------------
+int[] GetArray(int size)
+{
+ int[] arr = new int[size];
+for (int i = 0; i < size; i++)
+{
+    arr[i] = new Random().Next(101);
+}
+return arr;
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+SelectionSort(arr);
+Console.WriteLine($"{String.Join(" ", arr)}");
+
+
 
